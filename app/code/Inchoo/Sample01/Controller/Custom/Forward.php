c20590080d54ea48478877c62ab9225810f9e6bc
ivanklesic <ivan.klesic@gmail.com>
2021-08-06 09:41:06 +0200

    finished sample 1

---
+++ app/code/Inchoo/Sample01/Controller/Custom/Forward.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Inchoo\Sample01\Controller\Custom;
+
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\Controller\ResultFactory;
+
+class Forward implements ActionInterface
+{
+    /**
+     * @var ResultFactory
+     */
+    protected $resultFactory;
+
+    /**
+     * Index constructor.
+     * @param ResultFactory $resultFactory
+     */
+    public function __construct(ResultFactory $resultFactory)
+    {
+        $this->resultFactory = $resultFactory;
+    }
+
+    /**
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        /** @var \Magento\Framework\Controller\Result\Forward $resultForward */
+        $resultForward = $this->resultFactory->create(ResultFactory::TYPE_FORWARD);
+
+        $resultForward->setController('index');
+        return $resultForward->forward('index');
+    }
+}
---
+++ app/code/Inchoo/Sample01/Controller/Custom/Redirect.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Inchoo\Sample01\Controller\Custom;
+
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\Controller\ResultFactory;
+
+class Redirect implements ActionInterface
+{
+    /**
+     * @var ResultFactory
+     */
+    protected $resultFactory;
+
+    /**
+     * Index constructor.
+     * @param ResultFactory $resultFactory
+     */
+    public function __construct(ResultFactory $resultFactory)
+    {
+        $this->resultFactory = $resultFactory;
+    }
+
+    /**
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        /** @var \Magento\Framework\Controller\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+
+        return $resultRedirect->setPath('hello/index/old');
+    }
+}
