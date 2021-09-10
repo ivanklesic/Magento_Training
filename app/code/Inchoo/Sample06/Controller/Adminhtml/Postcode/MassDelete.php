dd7bc4bb799dc857f6b03a9aa48dd9960fa0bd74
ivanklesic <ivan.klesic@gmail.com>
2021-09-10 11:40:46 +0200

    PR fix

--- app/code/Inchoo/Sample06/Controller/Adminhtml/Postcode/MassDelete.php
+++ app/code/Inchoo/Sample06/Controller/Adminhtml/Postcode/MassDelete.php
@@ -1,17 +1,16 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Inchoo\Sample06\Controller\Adminhtml\Postcode;
 
+use Inchoo\Sample06\Model\PostcodeRepository;
+use Magento\Backend\App\Action;
 use Magento\Framework\App\Action\HttpPostActionInterface;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Backend\App\Action\Context;
 use Magento\Ui\Component\MassAction\Filter;
 use Inchoo\Sample06\Model\ResourceModel\Postcode\CollectionFactory;
 
-/**
- * Class MassDelete
- */
-class MassDelete extends \Magento\Backend\App\Action implements HttpPostActionInterface
+class MassDelete extends Action implements HttpPostActionInterface
 {
     /**
      * Authorization level of a basic admin session
@@ -31,14 +30,25 @@ class MassDelete extends \Magento\Backend\App\Action implements HttpPostActionIn
     protected $collectionFactory;
 
     /**
+     * @var PostcodeRepository
+     */
+    protected $postCodeRepository;
+
+    /**
      * @param Context $context
      * @param Filter $filter
      * @param CollectionFactory $collectionFactory
      */
-    public function __construct(Context $context, Filter $filter, CollectionFactory $collectionFactory)
+    public function __construct(
+        Context $context,
+        Filter $filter,
+        CollectionFactory $collectionFactory,
+        PostcodeRepository $postCodeRepository
+    )
     {
         $this->filter = $filter;
         $this->collectionFactory = $collectionFactory;
+        $this->postCodeRepository = $postCodeRepository;
         parent::__construct($context);
     }
 
@@ -53,8 +63,8 @@ class MassDelete extends \Magento\Backend\App\Action implements HttpPostActionIn
         $collection = $this->filter->getCollection($this->collectionFactory->create());
         $collectionSize = $collection->getSize();
 
-        foreach ($collection as $block) {
-            $block->delete();
+        foreach ($collection as $postCode) {
+            $this->postCodeRepository->delete($postCode);
         }
 
         $this->messageManager->addSuccessMessage(__('A total of %1 record(s) have been deleted.', $collectionSize));
